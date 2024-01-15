73d99376a75d14fc4b095f27ba50157a7332c62b
Muhammad Irfan <virtuoso.irfan@gmail.com>
2024-01-15 20:50:44 +0500

    minor change

--- multilabel_sentiment_analysis.py
+++ multilabel_sentiment_analysis.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -35,7 +37,27 @@ ConfusionMatrixDisplay.from_predictions(y_true, y_pred, display_labels=["positiv
 plt.savefig("graphs/multilabel_confusion_matrix.png")
 plt.close()
 
+plt.clf()
+# historgram
+counter = Counter(y_true)
+positive = counter[1]
+negative = counter[-1]
+neutral = counter[0]
 
+plt.figure(figsize=(10, 5))
+
+# making the bar chart on the data
+plt.bar(["positive", "neutral", "negative"], [positive, neutral, negative])
+# calling the function to add value labels
+addlabels(["positive", "neutral", "negative"], [positive, neutral, negative])
+# giving title to the plot
+plt.title("Dataset")
+# giving X and Y labels
+plt.xlabel("labels")
+plt.ylabel("no. of tweets")
+plt.savefig("graphs/multi_histogram.png")
+plt.close()
+plt.clf()
 # roc curve
 y_train = df["label"].apply(lambda x: id2label(x)).values.tolist()
 y_pred = df["my_sentiment"].apply(lambda x: id2label(x)).values.tolist()
